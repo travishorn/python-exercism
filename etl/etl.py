620bd88a61f42eb442205de51974f8f9b68ee544
Travis Horn <travis@travishorn.com>
2023-04-03 11:25:33 -0500

    Completed ETL

--- etl/etl.py
+++ etl/etl.py
@@ -1,2 +1,16 @@
 def transform(legacy_data):
-    pass
+    # Create an empty dictionary
+    result = {}
+
+    # Loop through all of the keys in `legacy_data`. Each key is a point value
+    for points in legacy_data:
+        # Each key (point value) corresponds to an array of letters. Loop
+        # through the array of letters
+        for letter in legacy_data[points]:
+            # Assign the point value we're current in to the key
+            # `letter.lower()` in our `result` dictionary. `.lower()` ensures
+            # that all letters in the result are lowercase
+            result[letter.lower()] = points
+
+    # Return the dictionary filled with letters and corresponding point values
+    return result;
